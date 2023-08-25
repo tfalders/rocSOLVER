1d4337370b287a24ee7dfaadd709fc52b1fb2aab
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2023-08-25 13:46:47 -0600

    Revert "rocblas trsm"
    This reverts commit ad16e025df34203a3bfabd9d2a87ba1e8fda8adc.

--- library/src/specialized/roclapack_trsm_specialized_kernels.hpp
+++ library/src/specialized/roclapack_trsm_specialized_kernels.hpp
@@ -652,10 +652,10 @@ rocblas_int rocsolver_trsm_blksize(const rocblas_int m, const rocblas_int n)
         blk = size[get_index(intervalsM, M, m)][get_index(intervalsN, N, n)];
     }
 
-    if(blk == 1)
+    if(blk <= 1)
         blk = std::min(m, 512);
 
-    return 0;
+    return blk;
 }
 
 /** complex type version **/
@@ -683,10 +683,10 @@ rocblas_int rocsolver_trsm_blksize(const rocblas_int m, const rocblas_int n)
         blk = size[get_index(intervalsM, M, m)][get_index(intervalsN, N, n)];
     }
 
-    if(blk == 1)
+    if(blk <= 1)
         blk = std::min(m, 512);
 
-    return 0;
+    return blk;
 }
 
 /** This function determine workspace size for the internal trsm **/
