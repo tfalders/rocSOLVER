1aec72ad1bc47b1e4d4e75cc6d4e70c99d11ca03
Troy Alderson <58866654+tfalders@users.noreply.github.com>
2025-06-30 16:20:21 -0600

    OpenMP test 3

--- library/src/auxiliary/rocauxiliary_stedc.hpp
+++ library/src/auxiliary/rocauxiliary_stedc.hpp
@@ -42,7 +42,9 @@
 
 #include <algorithm>
 
+#ifdef _OPENMP
 #include <omp.h>
+#endif
 
 ROCSOLVER_BEGIN_NAMESPACE
 
@@ -1425,7 +1427,9 @@ void stedc_mergeValues_host(const rocblas_int k,
                 {
                     if(i % 2 == 0)
                     {
+#ifdef _OPENMP
 #pragma omp parallel for
+#endif
                         for(int j = 0; j < dd / 2; j++)
                         {
                             if(tmpd[2 * j] > tmpd[2 * j + 1])
@@ -1438,7 +1442,9 @@ void stedc_mergeValues_host(const rocblas_int k,
                     }
                     else
                     {
+#ifdef _OPENMP
 #pragma omp parallel for
+#endif
                         for(int j = 0; j < (dd - 1) / 2; j++)
                         {
                             if(tmpd[2 * j + 1] > tmpd[2 * j + 2])
@@ -1457,7 +1463,9 @@ void stedc_mergeValues_host(const rocblas_int k,
 // eigenvalues (D - lambda_i) are updated while computing each eigenvalue.
 // This will prevent collapses and division by zero when an eigenvalue
 // is too close to a pole.
+#ifdef _OPENMP
 #pragma omp parallel for
+#endif
             for(int j = 1; j < sz; j++)
             {
                 for(int i = 0; i < dd; ++i)
@@ -1466,7 +1474,9 @@ void stedc_mergeValues_host(const rocblas_int k,
 
 // finally copy over all diagonal elements in ev. ev will be overwritten
 // by the new computed eigenvalues of the merged block
+#ifdef _OPENMP
 #pragma omp parallel for
+#endif
             for(int i = 0; i < sz; i++)
                 ev[i] = diag[i];
             /* ----------------------------------------------------------------- */
@@ -1476,7 +1486,9 @@ void stedc_mergeValues_host(const rocblas_int k,
             /* ----------------------------------------------------------------- */
             // each thread will find a different zero in parallel
             S a, b;
-#pragma omp parallel for
+            // #ifdef _OPENMP
+            // #pragma omp parallel for
+            // #endif
             for(int j = 0; j < sz; j++)
             {
                 printf("thread %i processing %i\n", omp_get_thread_num(), j);
@@ -1517,7 +1529,9 @@ void stedc_mergeValues_host(const rocblas_int k,
 
 // Re-scale vector Z to avoid bad numerics when an eigenvalue
 // is too close to a pole
+#ifdef _OPENMP
 #pragma omp parallel for
+#endif
             for(int i = 0; i < dd; i++)
             {
                 valf = 1;
